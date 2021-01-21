889f190c5cc23d5484136f4483905619268f56a7
elifoguzkan <oguzkanelif@gmail.com>
2021-01-21 23:33:02 +0300

    deneme
    deneme

---
+++ MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tvalue,Tkey>
+    {
+        Tvalue[] tvalues;
+        Tkey[] tkeys;
+
+        public MyDictionary()
+        {
+            tvalues = new Tvalue [0] ;
+            tkeys = new Tkey[0];
+        }
+        public void Add(Tvalue value, Tkey key)
+        {
+            Tvalue[] tempVal = tvalues;
+            Tkey[] tempKey = tkeys;
+            tvalues = new Tvalue[tvalues.Length + 1];
+            tkeys = new Tkey[tkeys.Length + 1];
+            for (int i = 0; i < tempVal.Length; i++)
+            {
+                tvalues[i] = tempVal[i];
+                tkeys[i] = tempKey[i];
+            }
+            tvalues[tvalues.Length - 1] = value;
+            tkeys[tkeys.Length - 1] = key;
+        }
+        public int Lenght
+        {
+            get { return tkeys.Length; }
+        }
+        public Tkey[] Keys
+        {
+            get { return tkeys; }
+        }
+        public Tvalue[] Values
+        {
+            get { return tvalues; }
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary <string, int> sehirPostaKodu = new Dictionary<string, int>();
+            sehirPostaKodu.Add("Kayseri", 38000);
+            sehirPostaKodu.Add("Samsun", 55000);
+            sehirPostaKodu.Add("İzmir", 35000);
+            sehirPostaKodu.Add("Sivas", 58000);
+            sehirPostaKodu.Add("Ankara", 06000);
+            sehirPostaKodu.Add("Mersin", 33000);
+            sehirPostaKodu.Add("Kırıkkale", 71000);
+            sehirPostaKodu.Add("Nevşehir", 50000);
+            sehirPostaKodu.Add("İstanbul", 34000);
+            sehirPostaKodu.Add("Adana", 01000);
+            sehirPostaKodu.Add("Kırşehir", 40000);
+            sehirPostaKodu.Add("Amasya", 05000);
+
+            foreach (var spkBul in sehirPostaKodu) //ekrana yazdırma
+            {
+                Console.WriteLine(spkBul);
+            }
+
+
+
+
+        }
+    }
+}
